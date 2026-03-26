a560981c526950e7a599ea7c321b4a0e75d12531
ynso-a8 <ynso1248@naver.com>
2026-03-26 19:15:03 +0900

    First commit

---
+++ src/test/java/Haedal/bootcamp/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Haedal.bootcamp.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
